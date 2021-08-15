17241f5e84cccdfc5d8e48596a284514e7120382
grace <grace.c.youngblood@gmail.com>
2021-08-15 19:22:29 -0400

    Use a render view to display tasks.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import { types } from "mobx-state-tree"
+import { types, applyPatch, onPatch } from "mobx-state-tree"
 import { values } from "mobx"
+import { observer } from "mobx-react"
 
 var Task = types.model({
   label: types.string,
   done: types.boolean,
-})
+}).views(self => ({
+  get display() {
+    return (
+      <div>
+        <input
+          type="checkbox"
+          checked={self.done}
+          onChange={(e) => applyPatch(self, {
+            op: "replace",
+            path: `./done`,
+            value: !self.done,
+          })}
+        />
+        {self.label}
+      </div>
+    )
+  }
+}))
 
 var Agenda = types.model({
   tasks: types.map(Task),
@@ -16,17 +34,16 @@ window.model = Agenda.create({
   }
 })
 
+// onPatch(window.model, patch => {
+//     console.info("Got change: ", patch)
+// })
+
 function App() {
   return (
     <>
-      {values(window.model.tasks).map(task => (
-        <div>
-          <checkbox checked={task.done} />
-          {task.label}
-        </div>
-      ))}
+      {values(window.model.tasks).map(task => task.display)}
     </>
   );
 }
 
-export default App;
+export default observer(App);
