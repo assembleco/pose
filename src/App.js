e02e6443db20784df7a06b902fe4f491825cad3d
grace <grace.c.youngblood@gmail.com>
2021-08-15 18:45:00 -0400

    Add a basic model.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import { types } from "mobx-state-tree"
+import { values } from "mobx"
+
+var Task = types.model({
+  label: types.string,
+  done: types.boolean,
+})
+
+var Agenda = types.model({
+  tasks: types.map(Task),
+})
+
+window.model = Agenda.create({
+  tasks: {
+    [Math.random()]: { label: "Have dinner", done: false },
+  }
+})
 
 function App() {
   return (
-    <div>
-    Hello Pose.
-    </div>
+    <>
+      {values(window.model.tasks).map(task => (
+        <div>
+          <checkbox checked={task.done} />
+          {task.label}
+        </div>
+      ))}
+    </>
   );
 }
 
