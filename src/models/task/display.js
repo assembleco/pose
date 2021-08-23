af5a7f37701afdede8f9c00d420cb66f6db7e788
grace <grace.c.youngblood@gmail.com>
2021-08-22 22:48:53 -0400

    Make `Task#display` an in-place editable model.

--- src/models/task/display.js
+++ src/models/task/display.js
@@ -1,17 +1,51 @@
+import React from "react"
 import { observer } from "mobx-react"
 import { applyPatch } from "mobx-state-tree"
+import { Icon } from "@iconify/react"
 
-export default observer(({ self }) => (
-  <div>
-    <input
-      type="checkbox"
-      checked={self.done}
-      onChange={(e) => applyPatch(self, {
-        op: "replace",
-        path: `./done`,
-        value: !self.done,
-      })}
-    />
-    {self.label}
-  </div>
-))
+export default observer(({ self }) => {
+  var [editing, setEditing] = React.useState(false)
+
+  return (
+    <div>
+      <input
+        type="checkbox"
+        checked={self.done}
+        onChange={(e) => applyPatch(self, {
+          op: "replace",
+          path: `./done`,
+          value: !self.done,
+        })}
+      />
+
+      {editing
+        ?
+        <>
+          <input
+            type="text"
+            value={self.label}
+            onChange={(e) => applyPatch(self, {
+              op: "replace",
+              path: "./label",
+              value: e.target.value,
+            })}
+          />
+          <Icon
+            icon="ci:check"
+            onClick={() => setEditing(false)}
+          />
+        </>
+
+        :
+        <>
+          {self.label}
+          <Icon
+            icon="ci:edit"
+            onClick={() => setEditing(true)}
+          />
+        </>
+
+      }
+    </div>
+  )
+})
