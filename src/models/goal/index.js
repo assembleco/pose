4b4fd561dbc40f25c763c2d344e3206fcebd43e1
grace <grace.c.youngblood@gmail.com>
2021-10-07 14:18:16 -0400

    Rename 'Model' in `src/models/goal/index`.

--- src/models/goal/index.js
+++ src/models/goal/index.js
@@ -3,10 +3,10 @@ import { types } from "mobx-state-tree"
 import changeable from "../../change"
 import loadDisplays from "../../dynamic/displays"
 
-var Task = types.model('task', {
+var Model = types.model('task', {
   key: types.identifierNumber,
   label: types.string,
   done: types.string,
 })
 
-export default changeable(loadDisplays(Task), 'changing')
+export default changeable(loadDisplays(Model), 'changing')
