e1618b5c7f206e961cc7b900f6a0bc1b16bf4561
grace <grace.c.youngblood@gmail.com>
2021-10-18 17:04:34 -0400

    Change `'task'` -> `'goal'`.

--- src/models/goal/index.js
+++ src/models/goal/index.js
@@ -3,7 +3,7 @@ import { types } from "mobx-state-tree"
 import changeable from "../../change"
 import loadDisplays from "../../dynamic/displays"
 
-var Model = types.model('task', {
+var Model = types.model('goal', {
   key: types.identifierNumber,
   label: types.string,
   done: types.string,
