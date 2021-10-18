8ab70a31ab17b88509c508b52866d799bda3f08a
grace <grace.c.youngblood@gmail.com>
2021-10-18 16:42:44 -0400

    repair `snake_case`

--- src/dynamic_displays.js
+++ src/dynamic_displays.js
@@ -42,7 +42,7 @@ var snake_case = (str) => (
   str && str
   .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
   .map(s => s.toLowerCase())
-  .join('_');
+  .join('_')
 )
 
 var loadDisplays = (model) => {
